cc5919de0f78b083be8fd7884c86fccdb579d60a
Royweru <royezi17@gmail.com>
2023-07-05 20:49:01 +0300

    new file:   src/app/awsome/page.tsx 	new file:   src/app/components/Main.tsx 	new file:   src/app/components/Nav.tsx 	modified:   src/app/page.tsx 	modified:   src/middleware.ts

---
+++ src/app/awsome/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const awsome = () => {
+  return (
+    <div>
+        <h1 className=' text-4xl text-red-400'>
+            YOU ARE AWSOME
+        </h1>
+    </div>
+  )
+}
+
+export default awsome
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
-import Image from 'next/image'
-
-export default function Home() {
+import Image from "next/image";
+import { Main } from "./components/Main";
+import { options } from "./api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+export default async function Home() {
+  const session = await getServerSession(options);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1 className=' text-3xl text-yellow'>
-          WELCOME
-        </h1>
+      <div className=" w-full flex items-center justify-between font-mono text-sm lg:flex">
+        {session ? (
+          <Main />
+        ) : (
+          <>
+            <h1 className=" text-3xl text-yellow-400">
+              You are currently not signed in
+            </h1>
+            <br />
+            <h2 className=" text-2xl font-bold font-mono text-red-900">
+              <a href="/api/auth/signin">SIGN IN</a>
+            </h2>
+          </>
+        )}
       </div>
     </main>
-  )
+  );
 }
